19a8a8f9b95090449aa3a6bdfac100f9214001f6
Jelena Planic <jelena.lela.planic@gmail.com>
2026-09-08 20:03:16 +0200

    add AppDbContext with DbSets

---
+++ MES.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using MES.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MES.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Machine> Machines => Set<Machine>();
+        public DbSet<WorkOrder> WorkOrders => Set<WorkOrder>();
+        public DbSet<Downtime> Downtimes => Set<Downtime>();
+        public DbSet<DowntimeReason> DowntimeReasons => Set<DowntimeReason>();
+        public DbSet<Defect> Defects => Set<Defect>();
+        public DbSet<DefectType> DefectTypes => Set<DefectType>();
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Shift> Shifts => Set<Shift>();
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
